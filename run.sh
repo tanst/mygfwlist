fb621563b651269316f3885da2120cb907345730
tanst <t@tanst.net>
2022-03-02 09:35:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 cd "$(dirname "$0")"
 gfwb64=$(curl -s -k https://raw.githubusercontent.com/Loyalsoldier/domain-list-custom/release/gfwlist.txt)
-gfwid64=$(curl -s -k https://raw.githubusercontent.com/tanst/mygfwlist/master/add.ini | sed 's/^/||/g')
+add=$(curl -s -k https://raw.githubusercontent.com/tanst/mygfwlist/master/add.ini | sed 's/^/||/g')
 del=$(curl -s -k https://raw.githubusercontent.com/tanst/mygfwlist/master/del.ini | sed 's/^/||/g')
 gfwd64=$(echo "${gfwb64}" | base64 -d)
-echo -e "${gfwd64}\n${gfwid64}" >temp.txt
-sort temp.txt | uniq >gfwlist.txt
+echo -e "${gfwd64}\n${add}" >temp.txt
+awk '!x[$0]++' temp.txt >gfwlist.txt
 for i in $del; do
     sed -i "/$i/d" gfwlist.txt
 done
-cat gfwlist.txt | base64 >mygfwlist.txt
+base64 gfwlist.txt > mygfwlist.txt
 rm temp.txt
